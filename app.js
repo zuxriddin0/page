372f8710ec0b6cf60882398551f2c6add24797dc
zuxriddin0 <rustamramazonov01@gmail.com>
2024-10-08 23:08:44 +0500

    message

---
+++ app.js
@@ -0,0 +1,19 @@
+// 
+const heroBtn = document.querySelectorAll('.hero_btn')
+// setTimeout(  ()=> {
+heroBtn.forEach (button  => {
+    button.addEventListener('click' , () => {
+alert('Coming soon . . .')
+    })
+})
+// },2000)
+
+// 
+
+const brandBtn = document.querySelectorAll('.brand_btn')
+brandBtn.forEach(element => {
+    element.addEventListener('click', () => {
+        alert('Coming soon . . .')
+    })
+});
+// 
